4be55d20f9486fa845cca0b7098752983ab37eb1
janavoborska <jana.dostalov@seznam.cz>
2021-05-15 17:33:20 +0200

    :reseni

--- src/index.jsx
+++ src/index.jsx
@@ -1,32 +1,48 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { render } from 'react-dom';
 import './style.css';
 
 const MenuItem = (props) => {
   return (
-    <a href="#" className="menu-item">
+    <a
+      onClick={() => props.onSelect(props.text)}
+      href="#"
+      className="menu-item"
+    >
       {props.text}
     </a>
   );
 };
 
 const App = () => {
+  const [menuOpened, setMenuOpened] = useState(false);
+  const handleOpened = () => {
+    setMenuOpened(!menuOpened); //znegovany stav, jinak pred tim setMenuOpened(true)
+  };
+
+  const [pageTitle, setPageTitle] = useState('Domů');
+
+  const handleSelectItem = (page) => {
+    setMenuOpened(false);
+    setPageTitle(page);
+  };
+
   return (
     <>
       <header>
-        <div className="menu">
-          <button className="menu__btn"></button>
+        <div className={menuOpened === true ? 'menu' : 'menu menu--closed'}>
+          <button onClick={handleOpened} className="menu__btn"></button>
           <div className="menu__items">
-            <MenuItem text="Domů" />
-            <MenuItem text="Naše nabídka" />
-            <MenuItem text="Náš tým" />
-            <MenuItem text="Blog" />
-            <MenuItem text="Kontakt" />
+            <MenuItem onSelect={handleSelectItem} text="Naše nabídka" />
+            <MenuItem onSelect={handleSelectItem} text="Náš tým" />
+            <MenuItem onSelect={handleSelectItem} text="Blog" />
+            <MenuItem onSelect={handleSelectItem} text="Domů" />
+            <MenuItem onSelect={handleSelectItem} text="Kontakt" />
           </div>
         </div>
       </header>
       <main>
-        <h1>Domů</h1>
+        <h1>{pageTitle}</h1>
       </main>
     </>
   );
